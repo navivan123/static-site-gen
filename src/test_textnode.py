401941226825f54f9e61dfc55f28ee6b90c079bf
navivan123 <ivanfran49er@gmail.com>
2024-08-10 11:33:05 -0400

    Finished Inline section.  Next commits will be markdown -> block section.

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,13 @@
-import unittest
+from textnode import TextNode,                \
+                     text_node_to_html_node,  \
+                     split_nodes_delimiter,   \
+                     extract_markdown_images, \
+                     extract_markdown_links,  \
+                     split_nodes_image,       \
+                     split_nodes_link,        \
+                     text_to_textnodes
 
-from textnode import TextNode, text_node_to_html_node
+import unittest
 
 
 class TestTextNode(unittest.TestCase):
@@ -54,41 +61,126 @@ class TestTextNode(unittest.TestCase):
         return self.assertEqual(f"{node}", "TextNode(, , None)")
     
     def test_conv_text(self):
-         node = TextNode("Hello!", "text")
-         html_n = text_node_to_html_node(node)
-         return self.assertEqual(html_n.to_html(), "Hello!")
+        node = TextNode("Hello!", "text")
+        html_n = text_node_to_html_node(node)
+        return self.assertEqual(html_n.to_html(), "Hello!")
 
     def test_conv_bold(self):
-         node = TextNode("Hello!", "bold")
-         html_n = text_node_to_html_node(node)
-         return self.assertEqual(html_n.to_html(), "<b>Hello!</b>")
+        node = TextNode("Hello!", "bold")
+        html_n = text_node_to_html_node(node)
+        return self.assertEqual(html_n.to_html(), "<b>Hello!</b>")
 
     def test_conv_italic(self):
-         node = TextNode("Hello!", "italic")
-         html_n = text_node_to_html_node(node)
-         return self.assertEqual(html_n.to_html(), "<i>Hello!</i>")
+        node = TextNode("Hello!", "italic")
+        html_n = text_node_to_html_node(node)
+        return self.assertEqual(html_n.to_html(), "<i>Hello!</i>")
 
     def test_conv_code(self):
-         node = TextNode("Hello!", "code")
-         html_n = text_node_to_html_node(node)
-         return self.assertEqual(html_n.to_html(), "<code>Hello!</code>")
+        node = TextNode("Hello!", "code")
+        html_n = text_node_to_html_node(node)
+        return self.assertEqual(html_n.to_html(), "<code>Hello!</code>")
 
     def test_conv_link(self):
-         node = TextNode("Click here to go to google", "link", "www.google.com")
-         html_n = text_node_to_html_node(node)
-         return self.assertEqual(html_n.to_html(), '<a href="www.google.com">Click here to go to google</a>')
+        node = TextNode("Click here to go to google", "link", "www.google.com")
+        html_n = text_node_to_html_node(node)
+        return self.assertEqual(html_n.to_html(), '<a href="www.google.com">Click here to go to google</a>')
 
     def test_conv_image(self):
-         node = TextNode("img-alt", "image", "www.google.com/img.img")
-         html_n = text_node_to_html_node(node)
-         return self.assertEqual(html_n.to_html(), '<img src="www.google.com/img.img" alt="img-alt">')
+        node = TextNode("img-alt", "image", "www.google.com/img.img")
+        html_n = text_node_to_html_node(node)
+        return self.assertEqual(html_n.to_html(), '<img src="www.google.com/img.img" alt="img-alt">')
 
     def test_conv_invalid_type(self):
-         return self.assertRaises(Exception, text_node_to_html_node, TextNode("", "", ""))
+        return self.assertRaises(Exception, text_node_to_html_node, TextNode("", "", ""))
 
     def test_conv_invalid_closure(self):
-         node = TextNode("", "bold", None)
-         return self.assertRaises(Exception, text_node_to_html_node, node)
+        node = TextNode("", "bold", None)
+        return self.assertRaises(Exception, text_node_to_html_node, node)
+
+    def test_delim_split_bold(self):
+        node = TextNode("**Bold!**", "text")
+        n_t  = split_nodes_delimiter([node], "**", "bold")
+        return self.assertEqual(text_node_to_html_node(n_t[0]).to_html(), "<b>Bold!</b>")
+
+    def test_delim_split_unmatching(self):
+        node = TextNode("**Bold!*", "text")
+        return self.assertRaises(Exception, split_nodes_delimiter, [node], "**", "bold")
+    
+    def test_delim_split_italic(self):
+        node = TextNode("*Brash!*", "text")
+        n_t  = split_nodes_delimiter([node], "*", "italic")
+        return self.assertEqual(text_node_to_html_node(n_t[0]).to_html(), "<i>Brash!</i>")
+
+    def test_delim_split_code(self):
+        node = TextNode("`Belongs!`", "text")
+        n_t  = split_nodes_delimiter([node], "`", "code")
+        return self.assertEqual(text_node_to_html_node(n_t[0]).to_html(), "<code>Belongs!</code>")
+
+    def test_delim_split_all(self):
+        n1 = TextNode("I have **Coders** and *maybe* **coders** and `possibly` **coders?** `possibly`", "text")
+        pass1 = split_nodes_delimiter([n1], "**", "bold")
+        pass2 = split_nodes_delimiter(pass1, "*", "italic")
+        pass3 = split_nodes_delimiter(pass2, "`", "code")
+        
+        string = ""
+        for node in pass3:
+            string += text_node_to_html_node(node).to_html()
+
+        return self.assertEqual(string, "I have <b>Coders</b> and <i>maybe</i> <b>coders</b> and <code>possibly</code> <b>coders?</b> <code>possibly</code>")
+
+    def test_delim_split_mult(self):
+        n1 = TextNode("**in**", "text")
+        n2 = TextNode("**the**", "text")
+        n3 = TextNode("**Trash!**", "text")
+
+        n_all = split_nodes_delimiter([n1, n2, n3], "**", "bold")
+        
+        string = ""
+        for node in n_all:
+            string += text_node_to_html_node(node).to_html()
+
+        return self.assertEqual(string, "<b>in</b><b>the</b><b>Trash!</b>")
+
+    def test_ex_mark_img(self):
+        return self.assertEqual(extract_markdown_images("a;lfgihawegfih ![shit](www.image.com/img.png) ![shit2](image.png)"), \
+                                                      [("shit","www.image.com/img.png"), ("shit2","image.png")]) 
+    
+    def test_ex_mark_link(self):
+        return self.assertEqual(extract_markdown_links("agwgwggfih  [googs](google.com)"), \
+                                                     [("googs","google.com")])
+
+    def test_split_node_img(self):
+        n1 = TextNode("[shit](link.com) when i ![shoot](shot.com) let ![shit](chuck.com) [fork](book.com)![sting](stang.com)", "text")
+        n2 = TextNode("more images: ![shoot](shot.com)", "text")
+        n3 = TextNode("just link: [shoot](shot.com)", "text")
+        n4 = TextNode("", "text")
+        n5 = TextNode("just text: text", "text")
+
+
+        n6 = split_nodes_image([n1, n2, n3, n4, n5])
+        
+        return self.assertEqual("[TextNode([shit](link.com) when i , text, None), TextNode(shoot, image, shot.com), TextNode( let , text, None), TextNode(shit, image, chuck.com), TextNode( [fork](book.com), text, None), TextNode(sting, image, stang.com), TextNode(more images: , text, None), TextNode(shoot, image, shot.com), TextNode(just link: [shoot](shot.com), text, None), TextNode(just text: text, text, None)]" , f'{n6}')
+    
+    def test_split_node_all(self):
+        self.maxDiff = None
+        n1 = TextNode("[shit](link.com) when i ![shoot](shot.com) let ![shit](chuck.com) [fork](book.com)![sting](stang.com)", "text")
+        n2 = TextNode("more images: ![shoot](shot.com)", "text")
+        n3 = TextNode("just link: [shoot](shot.com)", "text")
+        n4 = TextNode("", "text")
+        n5 = TextNode("just text: text", "text")
+
+        n6 = split_nodes_image([n1, n2, n3, n4, n5])
+        n7 = split_nodes_link(n6)
+        
+        return self.assertEqual("[TextNode(shit, link, link.com), TextNode( when i , text, None), TextNode(shoot, image, shot.com), TextNode( let , text, None), TextNode(shit, image, chuck.com), TextNode( , text, None), TextNode(fork, link, book.com), TextNode(sting, image, stang.com), TextNode(more images: , text, None), TextNode(shoot, image, shot.com), TextNode(just link: , text, None), TextNode(shoot, link, shot.com), TextNode(just text: text, text, None)]", f"{n7}")
+
+
+    def test_text_to_node(self):
+        n = text_to_textnodes("`I start with code` But continue ![fox](c.com) with the *never* **ending** ![relentless](frick.png) prrrsuit of the *maybe* `rich` individuals saying shit like [notimg](nimg.com) that really fricks up my **banana**")
+        print()
+        for node in n:
+            print(f"{node}")
+
 
     def test_print_all_convs(self):
         print()
--- src/textnode.py
+++ src/textnode.py
@@ -1,12 +1,14 @@
 from htmlnode import LeafNode
+import re
 
-text_type_text   = "text"
-text_type_bold   = "bold"
-text_type_italic = "italic"
-text_type_code   = "code"
-text_type_link   = "link"
-text_type_image  = "image"
+######### Global "constant" definitions #########
 
+text_type_text   = "text"   # Markdown: nothing, duh!
+text_type_bold   = "bold"   # Markdown: **
+text_type_italic = "italic" # Markdown: *
+text_type_code   = "code"   # Markdown: `
+text_type_link   = "link"   # Markdown: []()
+text_type_image  = "image"  # Markdown: ![]()
 
 tt_tag = { text_type_text   : None,   \
            text_type_bold   : "b",    \
@@ -32,10 +34,10 @@ tt_pro = { text_type_text   : "",   \
            text_type_image  : ["src", "alt"]
          }
 
-
-
+######### Text Node Class to break down Markup #########
 
 class TextNode:
+    
     def __init__(self, text, text_type, url = None):
         self.text = text
         self.text_type = text_type
@@ -46,6 +48,8 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+######### Helper Functions #########
 
 def text_node_to_html_node(text_node):
     if text_node.text_type not in tt_tag:
@@ -67,3 +71,125 @@ def text_node_to_html_node(text_node):
         v3 = {tt_pro[text_type_image][0] : text_node.url, tt_pro[text_type_image][1] : text_node.text}  
 
     return LeafNode(v1, v2, v3) 
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    
+    new_nodes = []
+    
+    for node in old_nodes:
+        
+        if node.text_type != "text":
+            new_nodes.append(node)
+            continue
+        
+        a = node.text.split(delimiter)
+        
+        if len(a) % 2 == 0:
+            raise Exception("Invalid Markdown Syntax! Section was not closed!")
+
+        for i, section in enumerate(a):
+            if section == "":
+                continue
+
+            if i % 2 == 0:
+                new_nodes.append(TextNode(a[i], "text"))
+
+            else:
+                new_nodes.append(TextNode(a[i], text_type))
+        
+    return new_nodes
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+            
+def extract_markdown_links(text):
+    return re.findall(r"\[(.*?)\]\((.*?)\)", text)
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        
+        if node.text_type != "text":
+            new_nodes.append(node)
+            continue
+
+        if node.text == "":
+            continue
+        
+        images = extract_markdown_images(node.text)
+        if images == []:
+            new_nodes.append(node)
+            continue
+
+        split_strings = []
+        
+        for i, image in enumerate(images):
+            if i == 0:
+                temp = node.text.split(f"![{image[0]}]({image[1]})", 1)
+            else:
+                temp = rest.split(f"![{image[0]}]({image[1]})", 1)
+
+            if len(temp) != 2:
+                raise ValueError("Invalid Markdown Syntax! Section was not closed")
+            
+            if temp[0] != "":
+                new_nodes.append(TextNode(temp[0], "text"))
+
+            new_nodes.append(TextNode(image[0], "image", image[1]))
+
+            rest = temp[1]
+
+        if rest != "":
+            new_nodes.append(TextNode(rest, "text"))
+
+    return new_nodes 
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        
+        if node.text_type != "text":
+            new_nodes.append(node)
+            continue
+
+        if node.text == "":
+            continue
+        
+        links = extract_markdown_links(node.text)
+        if links == []:
+            new_nodes.append(node)
+            continue
+
+        split_strings = []
+        
+        for i, link in enumerate(links):
+            if i == 0:
+                temp = node.text.split(f"[{link[0]}]({link[1]})", 1)
+            else:
+                temp = rest.split(f"[{link[0]}]({link[1]})", 1)
+
+            if len(temp) != 2:
+                raise ValueError("Invalid Markdown Syntax! Section was not closed")
+            
+            if temp[0] != "":
+                new_nodes.append(TextNode(temp[0], "text"))
+
+            new_nodes.append(TextNode(link[0], "link", link[1]))
+
+            rest = temp[1]
+
+        if rest != "":
+            new_nodes.append(TextNode(rest, "text"))
+
+    return new_nodes
+
+def text_to_textnodes(text):
+    n = [TextNode(text, "text")]
+    n = split_nodes_delimiter(n, "*", "italic")
+    n = split_nodes_delimiter(n, "**", "bold")
+    n = split_nodes_delimiter(n, "`", "code")
+    n = split_nodes_image(n)
+    return split_nodes_link(n)
